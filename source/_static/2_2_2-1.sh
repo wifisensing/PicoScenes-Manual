1606ceab08926e55851b596389ceb83272bd9ed0
lvdongtao <1367528058@qq.com>
2021-04-06 18:49:44 +0800

    update

--- source/_static/2_2_2-1.sh
+++ source/_static/2_2_2-1.sh
@@ -4,6 +4,6 @@ set -e
 
 phyID=2
 
-array_prepare_for_picoscenes ${phyID} "2412 HT20"
+array_prepare_for_picoscenes ${phyID} "5200 HT20"
 
-PicoScenes -d debug -i ${phyID} --mode logger
+PicoScenes -d debug -i ${phyID} --mode logger -- freq 4200e6 --rate 20e6

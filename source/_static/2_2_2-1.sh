bf2e84c273a86cf2d773f483dad2a0984adad895
renxincheng <13323919496@163.com>
2021-04-13 10:48:06 +0800

    fix bug

--- source/_static/2_2_2-1.sh
+++ source/_static/2_2_2-1.sh
@@ -6,4 +6,4 @@ phyID=2
 
 array_prepare_for_picoscenes ${phyID} "5200 HT20"
 
-PicoScenes -d debug -i ${phyID} --mode logger -- freq 4200e6 --rate 20e6
+PicoScenes -d debug -i ${phyID} --mode logger --freq 4200e6 --rate 20e6
